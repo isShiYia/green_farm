65d2326bcc12043c2f1344cea19e73564f03e3c9
choococo <choococo@163.com>
2019-09-11 23:24:12 +0800

    创建绿色农场项目001

--- src/main/java/cn/nchu/green_farm/entity/User.java
+++ src/main/java/cn/nchu/green_farm/entity/User.java
@@ -7,6 +7,15 @@ public class User {
 
     Integer id;
     String name;
+    String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getName() {
         return name;
