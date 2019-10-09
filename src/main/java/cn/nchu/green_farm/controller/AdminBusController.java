374c83cb84fe3d8ed42ef9339d2d4952beeaed7f
choococo <choococo@163.com>
2019-10-09 10:11:33 +0800

    管理员后台【1补充】

--- src/main/java/cn/nchu/green_farm/controller/AdminBusController.java
+++ src/main/java/cn/nchu/green_farm/controller/AdminBusController.java
@@ -1,8 +1,6 @@
 package cn.nchu.green_farm.controller;
 
-import cn.nchu.green_farm.entity.Address;
 import cn.nchu.green_farm.entity.Business;
-import cn.nchu.green_farm.service.IAddressService;
 import cn.nchu.green_farm.service.IAdminBusService;
 import cn.nchu.green_farm.util.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,24 +21,25 @@ public class AdminBusController extends  BaseController {
     private IAdminBusService adminBusService;
 
     // http://localhost:8080/admin/bus/list
-    @RequestMapping("/list")
+    @RequestMapping("/list") // 商家列表
     public ResponseResult<List<Business>> getListByUid() {
         List<Business> list = adminBusService.getListOfBusiness();
         return new ResponseResult<>(SUCCESS, list);
     }
 
     // http://localhost:8080/admin/bus/pass/1
-    @GetMapping("/pass/{id}")
+    @GetMapping("/pass/{id}") // 审核通过
     public ResponseResult<Void> handlePass(HttpSession session, @PathVariable("id") Integer id) {
-//        adminBusService.changeStatusPass(id,session.getAttribute("adminName").toString());
-        adminBusService.changeStatusPass(id,"admin07");
+        adminBusService.changeStatusPass(id,session.getAttribute("adminName").toString());
+//        adminBusService.changeStatusPass(id,"admin07");
         return new ResponseResult<>(SUCCESS);
     }
 
     // http://localhost:8080/admin/bus/no_pass/1
-    @GetMapping("no_pass/{id}")
+    @GetMapping("no_pass/{id}") // 审核不通过
     public ResponseResult<Void> handleNoPass(HttpSession session, @PathVariable("id") Integer id) {
-        adminBusService.changeStatusNoPass(id, "admin07");
+        adminBusService.changeStatusNoPass(id, session.getAttribute("adminName").toString());
+//        adminBusService.changeStatusNoPass(id, "admin07");
         return new ResponseResult<>(SUCCESS);
     }
 
