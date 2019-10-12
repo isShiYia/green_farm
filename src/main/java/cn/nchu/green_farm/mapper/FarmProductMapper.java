fed0d53397449d290fc687db73f12911a501a3e8
shidadaa <2644108676@qq.com>
2019-10-12 15:14:22 +0800

    农产品上传业务

---
+++ src/main/java/cn/nchu/green_farm/mapper/FarmProductMapper.java
@@ -0,0 +1,14 @@
+package cn.nchu.green_farm.mapper;
+
+import cn.nchu.green_farm.entity.FarmProduct;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FarmProductMapper {
+    /**
+     *
+     * @param farmProduct :农产品信息
+     * @return：数据库受影响行数
+     */
+    Integer addnew(FarmProduct farmProduct);
+}
